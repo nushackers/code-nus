d08f0471ceb8f5fdb664672252f6869a10297603
Yujian <yyjhao@gmail.com>
2014-10-23 16:36:40 +0800

    image support

--- app/scripts/ui/ProjectItem.js
+++ app/scripts/ui/ProjectItem.js
@@ -9,31 +9,38 @@ var ProjectItem = React.createClass({
         var project = this.props.project;
         return (
             /* jshint trailing:false, quotmark:false, newcap:false */
-            <div className="project">
-                <header>
-                    <div className="title">
-                        {project.title}
+            <div className={"project" + (project.image ? "" : " no-image")}>
+                <div className="text">
+                    <header>
+                        <div className="title">
+                            {project.title}
+                        </div>
+                        <div className="author">
+                            <a href={project.author.href}>{project.author.name}</a>
+                        </div>
+                    </header>
+                    <div className="body">
+                        <div className="description">
+                            {project.description}
+                        </div>
                     </div>
-                    <div className="author">
-                        <a href={project.author.href}>{project.author.name}</a>
+                    <div className="links">
+                        <a href={project.repository.href}>Repository</a>
+                        { project.release || project.homepage ?
+                            <a href={(project.release && project.release.href) || project.homepage}>Link</a>
+                            : []
+                        }
                     </div>
-                </header>
-                <div className="body">
-                    <div className="description">
-                        {project.description}
+                    <div className="stats">
+                        <div className="stars">
+                            {project.stars}
+                        </div>
                     </div>
                 </div>
-                <div className="links">
-                    <a href={project.repository.href}>Repository</a>
-                    { project.release || project.homepage ?
-                        <a href={(project.release && project.release.href) || project.homepage}>Link</a>
-                        : []
+                <div className="image">
+                    { project.image ?
+                    <img src={project.image} /> : []
                     }
-                </div>
-                <div className="stats">
-                    <div className="stars">
-                        {project.stars}
-                    </div>
                 </div>
             </div>
         );
