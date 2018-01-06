84c1b035971b6171e153cced7195927ea2d2c77f
li-kai <falconets25601@hotmail.com>
2018-01-07 01:15:44 +0800

    Add more config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,21 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': WARN_IN_DEV,
+    // Allow hidden functions
+    "no-underscore-dangle": [WARN_IN_DEV, { "allowAfterThis": true }],
+    // Let git handle the linebreaks instead.
+    'linebreak-style': 'off',
   },
+  overrides: [
+    {
+      files: ['**/*.test.{js,jsx}', '**/__mocks__/*.{js,jsx}'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        // Allow hidden functions for testing
+        "no-underscore-dangle": 'off',
+      }
+    },
+  ],
 };
