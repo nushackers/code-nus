14cd5ef9739e260502a2248158d8153a0dcbdb48
li-kai <falconets25601@hotmail.com>
2018-01-07 12:19:46 +0800

    Begin with preact cli for front end

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 const WARN_IN_DEV = process.env.NODE_ENV === 'production' ? 'error' : 'warn';
 
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['synacor', 'airbnb-base', 'prettier'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': WARN_IN_DEV,
